5a0539510aa410d34c763617def43c181e703df7
dan <eudangeld@gmail.com>
2022-08-23 06:23:33 -0300

    chore: move test files

---
+++ lib/features/meals/domain/usecases/filter_by_categorie.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:will_flutter/core/error/failures.dart';
+import 'package:will_flutter/features/meals/domain/entities/meal_filtered.dart';
+import 'package:will_flutter/features/meals/domain/repositories/meals_repository.dart';
+
+class FilterByCategorie {
+  final MealRepository repository;
+
+  FilterByCategorie(this.repository);
+
+  Future<Either<Failure, List<Mealfiltered>>> call(String categorieName) async {
+    return await repository.filterByCategory(categorieName);
+  }
+}
--- test/features/meals/domain/usecases/filter_by_category_test.dart
+++ test/features/meals/domain/usecases/filter_by_category_test.dart
@@ -2,21 +2,11 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:will_flutter/core/error/failures.dart';
 import 'package:will_flutter/features/meals/domain/entities/meal_filtered.dart';
 import 'package:will_flutter/features/meals/domain/repositories/meals_repository.dart';
+import 'package:will_flutter/features/meals/domain/usecases/filter_by_categorie.dart';
 
 import 'get_categories_dart_test.mocks.dart';
-
-class FilterByCategorie {
-  final MockMealRepository repository;
-
-  FilterByCategorie(this.repository);
-
-  Future<Either<Failure, List<Mealfiltered>>> call(String categorieName) async {
-    return await repository.filterByCategory(categorieName);
-  }
-}
 
 @GenerateMocks([MealRepository])
 void main() {
