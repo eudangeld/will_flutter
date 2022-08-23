5a3beec65d21b8d1e8a9b49584d7f80f471c7a18
dan <eudangeld@gmail.com>
2022-08-23 13:23:54 -0300

    chore: update test

--- test/features/meals/data/repositories/meals_repositorie_implementation_test.dart
+++ test/features/meals/data/repositories/meals_repositorie_implementation_test.dart
@@ -1,50 +1,14 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:will_flutter/core/error/exceptions.dart';
 import 'package:will_flutter/core/error/failures.dart';
-import 'package:will_flutter/core/platform/connectivity_info.dart';
 import 'package:will_flutter/features/meals/data/models/categories_list_model.dart';
 import 'package:will_flutter/features/meals/data/models/meals_list_model.dart';
-import 'package:will_flutter/features/meals/data/sources/meals_remote_datasource.dart';
+import 'package:will_flutter/features/meals/data/repositories/meals_repository_implementation.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
 import 'package:will_flutter/features/meals/domain/entities/meal.dart';
-import 'package:will_flutter/features/meals/domain/repositories/meals_repository.dart';
 
 import '../../../../mocks/build_runner.mocks.dart';
-
-class MealRepositoryImplementation implements MealRepository {
-  final ConnectivityInfo networkInfo;
-  final MealsRemoteDataSource remoteDataSource;
-
-  MealRepositoryImplementation({
-    required this.networkInfo,
-    required this.remoteDataSource,
-  });
-
-  @override
-  Future<Either<Failure, List<Meal>>> filterByCategory(
-      {required String category}) async {
-    await networkInfo.hasConnection;
-
-    try {
-      return Right(await remoteDataSource.filterByCategory(category: category));
-    } on ServerException {
-      return Left(ServerFailure());
-    }
-  }
-
-  @override
-  Future<Either<Failure, List<Categorie>>> getCategories() async {
-    await networkInfo.hasConnection;
-
-    try {
-      return Right(await remoteDataSource.getCategories());
-    } on ServerException {
-      return Left(ServerFailure());
-    }
-  }
-}
 
 void main() {
   late MealRepositoryImplementation sutRepository;
