1fb509888752a3b49a26e10e779444939d82ee7f
dan <eudangeld@gmail.com>
2022-08-23 10:23:16 -0300

    feat: return categories with connectiondone

--- lib/features/meals/domain/repositories/meals_repository.dart
+++ lib/features/meals/domain/repositories/meals_repository.dart
@@ -1,5 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
 import 'package:will_flutter/core/error/failures.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
 import 'package:will_flutter/features/meals/domain/entities/meal.dart';
@@ -7,5 +6,5 @@ import 'package:will_flutter/features/meals/domain/entities/meal.dart';
 abstract class MealRepository {
   Future<Either<Failure, List<Categorie>>> getCategories();
   Future<Either<Failure, List<Meal>>> filterByCategory(
-      {@required String category});
+      {required String category});
 }
--- test/features/meals/data/repositories/meals_repositorie_implementation_test.dart
+++ test/features/meals/data/repositories/meals_repositorie_implementation_test.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:will_flutter/core/error/failures.dart';
 import 'package:will_flutter/core/platform/connectivity_info.dart';
+import 'package:will_flutter/features/meals/data/models/categories_list_model.dart';
 import 'package:will_flutter/features/meals/data/sources/meals_remote_datasource.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
 import 'package:will_flutter/features/meals/domain/entities/meal.dart';
@@ -21,16 +23,24 @@ class MealRepositoryImplementation implements MealRepository {
   @override
   Future<Either<Failure, List<Meal>>> filterByCategory(
       {required String category}) async {
-    // TODO: implement filterByCategory
-    throw UnimplementedError();
+    final hasConnection = await networkInfo.hasConnection;
+    if (!hasConnection) {
+      print('Have no connection');
+    }
 
-    Future.value();
+    return const Right([]);
+
+    // return Right(await remoteDataSource.filterByCategory(category: category));
   }
 
   @override
-  Future<Either<Failure, List<Categorie>>> getCategories() {
-    // TODO: implement getCategories
-    throw UnimplementedError();
+  Future<Either<Failure, List<Categorie>>> getCategories() async {
+    final hasConnection = await networkInfo.hasConnection;
+    if (!hasConnection) {
+      print('Have no connection');
+    }
+
+    return Right(await remoteDataSource.getCategories());
   }
 }
 
@@ -44,5 +54,30 @@ void main() {
     dataSource = MockMealsRemoteDataSource();
     sutRepository = MealRepositoryImplementation(
         networkInfo: netInfo, remoteDataSource: dataSource);
+  });
+
+  group('Categories With connection', () {
+    const tCategorie = Categorie(
+        id: '1',
+        title: 'Testcategorie',
+        thumbnail: 'https://www.themealdb.com/images/category/beef.png',
+        description: 'An categories description');
+
+    final categorieModel = CategoriesListModel([tCategorie]);
+
+    setUp(() {
+      when(netInfo.hasConnection).thenAnswer((_) async => true);
+    });
+
+    test('Should return categories data remote source ', () async {
+      when(dataSource.getCategories())
+          .thenAnswer((_) async => categorieModel.mealCategories);
+      final result = await sutRepository.getCategories();
+      verify(netInfo.hasConnection);
+      result.fold(
+        ((_) => {}),
+        (rightResul) => expect(rightResul, [tCategorie]),
+      );
+    });
   });
 }
