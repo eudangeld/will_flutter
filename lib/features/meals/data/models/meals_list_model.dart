7c81305eb9870c1ba707e58a844f6bfa438cf35c
dan <eudangeld@gmail.com>
2022-08-23 10:33:23 -0300

    feat: created meals list mode

---
+++ lib/features/meals/data/models/meals_list_model.dart
@@ -0,0 +1,20 @@
+import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
+import 'package:will_flutter/features/meals/domain/entities/meal.dart';
+
+class MealsListModel {
+  final List<Meal> meals;
+
+  factory MealsListModel.fromJson(Map<String, dynamic> categorieListJson) {
+    final parsedMeals = categorieListJson['meals'] as List;
+    final mappedMeals = parsedMeals
+        .map<Meal>((categorie) => Meal(
+            id: categorie['idMeal'],
+            title: categorie['strMeal'],
+            thumbnail: categorie['strMealThumb']))
+        .toList();
+
+    return MealsListModel(mappedMeals);
+  }
+
+  MealsListModel(this.meals);
+}
