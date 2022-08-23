2275675a08c5ffe2094d6093e40a3d29402eaf1c
dan <eudangeld@gmail.com>
2022-08-22 22:40:08 -0300

    fix: update Meal repository retrn type \nusing monads

--- lib/features/meals/domain/repositories/meals_repository.dart
+++ lib/features/meals/domain/repositories/meals_repository.dart
@@ -1,4 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:will_flutter/core/error/failures.dart';
+import 'package:will_flutter/features/meals/domain/entities/meal_categorie.dart';
+import 'package:will_flutter/features/meals/domain/entities/meal_filtered.dart';
+
 abstract class MealRepository {
-  getCategories();
-  filterByCategory(String category);
+  Future<Either<Failure, List<MealCategorie>>> getCategories();
+  Future<Either<Failure, List<Mealfiltered>>> filterByCategory(String category);
 }
