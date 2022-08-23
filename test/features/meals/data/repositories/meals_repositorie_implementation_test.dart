d5b15866942ff3b0a7e302b3d43c0366d3ff1112
dan <eudangeld@gmail.com>
2022-08-23 10:44:19 -0300

    feat: implement the good path for meals list too

--- test/features/meals/data/repositories/meals_repositorie_implementation_test.dart
+++ test/features/meals/data/repositories/meals_repositorie_implementation_test.dart
@@ -4,6 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:will_flutter/core/error/failures.dart';
 import 'package:will_flutter/core/platform/connectivity_info.dart';
 import 'package:will_flutter/features/meals/data/models/categories_list_model.dart';
+import 'package:will_flutter/features/meals/data/models/meals_list_model.dart';
 import 'package:will_flutter/features/meals/data/sources/meals_remote_datasource.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
 import 'package:will_flutter/features/meals/domain/entities/meal.dart';
@@ -28,9 +29,7 @@ class MealRepositoryImplementation implements MealRepository {
       print('Have no connection');
     }
 
-    return const Right([]);
-
-    // return Right(await remoteDataSource.filterByCategory(category: category));
+    return Right(await remoteDataSource.filterByCategory(category: category));
   }
 
   @override
@@ -81,27 +80,26 @@ void main() {
     });
   });
   group('Filter meals by categorie with connection', () {
-    const tTitleFilter = 'TestCategorie title';
-
-    const tFiltered = Meal(
+    const tMeal = Meal(
         id: '1',
-        title: 'Result filtered',
+        title: 'Testcmeal',
         thumbnail: 'https://www.themealdb.com/images/category/beef.png');
 
-    const tResult = [tFiltered];
+    final mealsModel = MealsListModel([tMeal]);
 
     setUp(() {
       when(netInfo.hasConnection).thenAnswer((_) async => true);
     });
 
-    test('Should return categories data remote source ', () async {
-      when(dataSource.getCategories())
-          .thenAnswer((_) async => categorieModel.mealCategories);
-      final result = await sutRepository.getCategories();
+    test('Should return meals list ', () async {
+      when(dataSource.filterByCategory(category: tMeal.title))
+          .thenAnswer((_) async => mealsModel.meals);
+      final result =
+          await sutRepository.filterByCategory(category: tMeal.title);
       verify(netInfo.hasConnection);
       result.fold(
         ((_) => {}),
-        (rightResul) => expect(rightResul, [tCategorie]),
+        (rightResul) => expect(rightResul[0], tMeal),
       );
     });
   });
