690262c793d44faa0361c8d1648bc9132a13c6d5
dan <eudangeld@gmail.com>
2022-08-24 11:23:33 -0300

    chore: update bloc semantic

--- lib/features/meals/presentation/bloc/categories_bloc.dart
+++ lib/features/meals/presentation/bloc/categories_bloc.dart
@@ -5,7 +5,7 @@ part 'categories_event.dart';
 part 'categories_state.dart';
 
 class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
-  CategoriesBloc() : super(CategoriesInitial()) {
+  CategoriesBloc() : super(CatgoriesEmptyState()) {
     on<CategoriesEvent>((event, emit) {
       // TODO: implement event handler
     });
