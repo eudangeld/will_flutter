96d1288d35d5fbb04ca619c9974dc4264860a2da
dan <eudangeld@gmail.com>
2022-08-24 11:17:10 -0300

    feat: created bloc states

--- lib/features/meals/presentation/bloc/meals_state.dart
+++ lib/features/meals/presentation/bloc/meals_state.dart
@@ -2,9 +2,15 @@ part of 'meals_bloc.dart';
 
 abstract class MealsState extends Equatable {
   const MealsState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class MealsInitial extends MealsState {}
+class EmptyState extends MealsState {}
+
+class LoadingState extends MealsState {}
+
+class LoadedState extends MealsState {}
+
+class ErrorState extends MealsState {}
