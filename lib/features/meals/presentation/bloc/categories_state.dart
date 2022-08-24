dc70d751dadf401fef23fb5e20273b987d68b746
dan <eudangeld@gmail.com>
2022-08-24 11:23:23 -0300

    feat: categories events

--- lib/features/meals/presentation/bloc/categories_state.dart
+++ lib/features/meals/presentation/bloc/categories_state.dart
@@ -2,9 +2,15 @@ part of 'categories_bloc.dart';
 
 abstract class CategoriesState extends Equatable {
   const CategoriesState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class CategoriesInitial extends CategoriesState {}
+class CatgoriesEmptyState extends CategoriesState {}
+
+class CatgoriesLoadingState extends CategoriesState {}
+
+class CatgoriesLoadedState extends CategoriesState {}
+
+class CatgoriesErrorState extends CategoriesState {}
