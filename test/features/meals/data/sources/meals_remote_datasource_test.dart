558cfa84a45f80e4116b85d663be339c1d5d6fdd
dan <eudangeld@gmail.com>
2022-08-24 10:56:21 -0300

    feat: impement meals list response tests

--- test/features/meals/data/sources/meals_remote_datasource_test.dart
+++ test/features/meals/data/sources/meals_remote_datasource_test.dart
@@ -6,6 +6,7 @@ import 'package:http_mock_adapter/http_mock_adapter.dart';
 import 'package:will_flutter/core/error/exceptions.dart';
 import 'package:will_flutter/features/meals/data/sources/meals_remote_datasource.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
+import 'package:will_flutter/features/meals/domain/entities/meal.dart';
 
 import '../../../../fixture_values/file_value_reader.dart';
 
@@ -32,9 +33,9 @@ void main() {
 
     test('Should return the categorie list after a sucses API response',
         () async {
-      const accountRoute = '/categories.php';
+      const tRoute = '/categories.php';
       dioAdapter.onGet(
-        accountRoute,
+        tRoute,
         (server) =>
             server.reply(200, jsonDecode(fileReader('categories.json'))),
       );
@@ -47,12 +48,47 @@ void main() {
     test(
         'Should throw a ServerException if somethng went wrong with API request',
         () async {
-      const accountRoute = '/categories.php';
-      dioAdapter.onGet(accountRoute, (server) => server.reply(404, {}));
+      const tRoute = '/categories.php';
+      dioAdapter.onGet(tRoute, (server) => server.reply(404, {}));
 
       final call = sut.getCategories;
 
       expect(() => call(), throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+  group('Meals list data source test group', () {
+    const baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+    const tMealRequestCategorie = 'Test categorie';
+
+    setUp(() {
+      dio = Dio(BaseOptions(baseUrl: baseUrl));
+      dioAdapter =
+          DioAdapter(dio: dio, matcher: const FullHttpRequestMatcher());
+      sut = MealsRemotaDataSourceImplementation(dio);
+    });
+
+    test('Should return the meals list', () async {
+      const tRoute = '/filter.php?c=$tMealRequestCategorie';
+      dioAdapter.onGet(
+        tRoute,
+        (server) => server.reply(200, jsonDecode(fileReader('meals.json'))),
+      );
+
+      final result =
+          await sut.filterByCategory(category: tMealRequestCategorie);
+
+      expect(result, isA<List<Meal>>());
+    });
+    test(
+        'Should throw a ServerException if something went wrong with API request',
+        () async {
+      const tRoute = '/filter.php?c=$tMealRequestCategorie';
+      dioAdapter.onGet(tRoute, (server) => server.reply(404, {}));
+
+      final call = sut.filterByCategory;
+
+      expect(() => call(category: tMealRequestCategorie),
+          throwsA(const TypeMatcher<ServerException>()));
     });
   });
 }
