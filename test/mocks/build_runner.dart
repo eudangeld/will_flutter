afa9a1f7f5e102ce2ab2dc2f5da165f2efb3510d
dan <eudangeld@gmail.com>
2022-08-24 14:15:30 -0300

    feat: mock ctegorie use case for null safety

--- test/mocks/build_runner.dart
+++ test/mocks/build_runner.dart
@@ -3,11 +3,13 @@ import 'package:mockito/annotations.dart';
 import 'package:will_flutter/core/platform/connectivity_info.dart';
 import 'package:will_flutter/features/meals/data/sources/meals_remote_datasource.dart';
 import 'package:will_flutter/features/meals/domain/repositories/meals_repository.dart';
+import 'package:will_flutter/features/meals/domain/usecases/get_categories.dart';
 
 @GenerateMocks([], customMocks: [
   MockSpec<MealRepository>(as: #MockMealRepository),
   MockSpec<ConnectivityInfo>(as: #MockConnectivityInfo),
   MockSpec<MealsRemoteDataSource>(as: #MockMealsRemoteDataSource),
   MockSpec<InternetConnectionChecker>(as: #MockInternetConnectionChecker),
+  MockSpec<GetCategories>(as: #MockGetCategorieUseCase),
 ])
 void main() {}
