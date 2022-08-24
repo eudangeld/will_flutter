95e594534d56f07de8149436bc882d83435ecba6
dan <eudangeld@gmail.com>
2022-08-24 11:17:18 -0300

    chore: change dafult state

--- lib/features/meals/presentation/bloc/meals_bloc.dart
+++ lib/features/meals/presentation/bloc/meals_bloc.dart
@@ -5,7 +5,7 @@ part 'meals_event.dart';
 part 'meals_state.dart';
 
 class MealsBloc extends Bloc<MealsEvent, MealsState> {
-  MealsBloc() : super(MealsInitial()) {
+  MealsBloc() : super(EmptyState()) {
     on<MealsEvent>((event, emit) {
       // TODO: implement event handler
     });
