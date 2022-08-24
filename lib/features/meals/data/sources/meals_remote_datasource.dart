5cef61f0981881f5b2c3d2ba9bc0d961c405edf5
dan <eudangeld@gmail.com>
2022-08-24 10:56:12 -0300

    feat: implement meals data source response

--- lib/features/meals/data/sources/meals_remote_datasource.dart
+++ lib/features/meals/data/sources/meals_remote_datasource.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:will_flutter/core/error/exceptions.dart';
 import 'package:will_flutter/features/meals/data/models/categories_list_model.dart';
+import 'package:will_flutter/features/meals/data/models/meals_list_model.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
 import 'package:will_flutter/features/meals/domain/entities/meal.dart';
 
@@ -13,11 +14,19 @@ class MealsRemotaDataSourceImplementation implements MealsRemoteDataSource {
   final Dio httpClient;
   final baseUrl = 'https://www.themealdb.com/api/json/v1/1';
 
-  MealsRemotaDataSourceImplementation(this.httpClient) {}
+  MealsRemotaDataSourceImplementation(this.httpClient) {
+    //TODO [Maybe] define env url's
+    httpClient.options.baseUrl = baseUrl;
+  }
 
   @override
-  Future<List<Meal>> filterByCategory({required String category}) {
-    return Future.value([]);
+  Future<List<Meal>> filterByCategory({required String category}) async {
+    try {
+      final response = await httpClient.get('/filter.php?c=$category');
+      return MealsListModel.fromJson(response.data).meals;
+    } on DioError catch (e) {
+      throw ServerException(e.message);
+    }
   }
 
   @override
@@ -25,8 +34,8 @@ class MealsRemotaDataSourceImplementation implements MealsRemoteDataSource {
     try {
       final response = await httpClient.get('/categories.php');
       return CategoriesListModel.fromJson(response.data).mealCategories;
-    } on DioError {
-      throw ServerException('message');
+    } on DioError catch (e) {
+      throw ServerException(e.message);
     }
   }
 }
