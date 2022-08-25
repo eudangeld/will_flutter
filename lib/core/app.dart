416fc28f9612b155326b228b847a7c8436d17b61
dan <eudangeld@gmail.com>
2022-08-25 09:18:38 -0300

    chore: remove unecessariochild declartion for multi bloc prov

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:will_flutter/core/routes/routes.dart';
 import 'package:will_flutter/features/meals/presentation/bloc/categories/categories_bloc.dart';
 import 'package:will_flutter/di_container.dart';
+import 'package:will_flutter/features/meals/presentation/bloc/filtered_meals/filter_bloc.dart';
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
@@ -13,9 +14,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-            create: (context) => serviceLocator<CategoriesBloc>(),
-            child: Container()),
+        BlocProvider(create: (context) => serviceLocator<CategoriesBloc>()),
+        BlocProvider(create: (context) => serviceLocator<FilterBloc>()),
       ],
       child: MaterialApp(routes: routes.routesBuilder),
     );
