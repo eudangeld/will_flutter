ef03cdb5556d39e866bcf98f2a8fde8c301e471a
dan <eudangeld@gmail.com>
2022-08-25 08:45:22 -0300

    feat: add an empty filtered screen

---
+++ lib/features/meals/presentation/pages/filtered_page_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FilteredPage extends StatelessWidget {
+  const FilteredPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
