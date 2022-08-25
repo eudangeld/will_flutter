0df59bb129019bc1219f7ee735db676aff64bd3a
dan <eudangeld@gmail.com>
2022-08-24 22:49:20 -0300

    feat: update dependencies

--- lib/features/meals/presentation/bloc/categories/categories_bloc.dart
+++ lib/features/meals/presentation/bloc/categories/categories_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:will_flutter/features/meals/domain/entities/categorie.dart';
 import 'package:will_flutter/features/meals/domain/usecases/get_categories.dart';
 
