7f9ca8b7420598df28d5d2d9c7edfe12a44dc6a9
dan <eudangeld@gmail.com>
2022-08-25 08:44:18 -0300

    fix: remove const form top level

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:will_flutter/core/app.dart';
-import 'package:will_flutter/injection_container.dart' as di;
+import 'package:will_flutter/di_container.dart' as di;
 
 //enjoy the silence
 void main() {
   di.initDependencies();
-  runApp(const App());
+  runApp(App());
 }
