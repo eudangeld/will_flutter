6eb3383c33ac13a2720541c26bb1d03db617fc2b
dan <eudangeld@gmail.com>
2022-08-25 07:54:08 -0300

    feat: put top level blocProvider for categories

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:will_flutter/features/meals/presentation/bloc/categories/categories_bloc.dart';
 import 'package:will_flutter/features/meals/presentation/pages/categories_page.dart';
+import 'package:will_flutter/injection_container.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CategoriesPage(),
+    return BlocProvider(
+      create: (context) => serviceLocator<CategoriesBloc>(),
+      child: MaterialApp(
+        home: CategoriesPage(),
+      ),
     );
   }
 }
