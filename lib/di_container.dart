6ae5b47a40f222e5a8e0c15c2a6237638379cc9d
dan <eudangeld@gmail.com>
2022-08-25 09:24:22 -0300

    feat: dependencies for filter mels

--- lib/di_container.dart
+++ lib/di_container.dart
@@ -5,21 +5,20 @@ import 'package:will_flutter/core/platform/connectivity_info.dart';
 import 'package:will_flutter/features/meals/data/repositories/meals_repository_implementation.dart';
 import 'package:will_flutter/features/meals/data/sources/meals_remote_datasource.dart';
 import 'package:will_flutter/features/meals/domain/repositories/meals_repository.dart';
+import 'package:will_flutter/features/meals/domain/usecases/filter_by_categorie.dart';
 import 'package:will_flutter/features/meals/domain/usecases/get_categories.dart';
 import 'package:will_flutter/features/meals/presentation/bloc/categories/categories_bloc.dart';
+import 'package:will_flutter/features/meals/presentation/bloc/filtered_meals/filter_bloc.dart';
 
 final serviceLocator = GetIt.instance;
 
 void initDependencies() {
+  commonsDependencies();
   categoriesBloc();
+  filterMealBloc();
 }
 
-void categoriesBloc() {
-  serviceLocator
-      .registerFactory<CategoriesBloc>(() => CategoriesBloc(serviceLocator()));
-
-  serviceLocator.registerLazySingleton(() => GetCategories(serviceLocator()));
-
+void commonsDependencies() {
   final connectionChecker = InternetConnectionCheckerPlus();
   serviceLocator.registerLazySingleton(() => connectionChecker);
 
@@ -37,4 +36,19 @@ void categoriesBloc() {
 
   serviceLocator.registerLazySingleton<MealsRemoteDataSource>(
       () => MealsRemotaDataSourceImplementation(serviceLocator()));
+}
+
+void categoriesBloc() {
+  serviceLocator
+      .registerFactory<CategoriesBloc>(() => CategoriesBloc(serviceLocator()));
+
+  serviceLocator.registerLazySingleton(() => GetCategories(serviceLocator()));
+}
+
+void filterMealBloc() {
+  serviceLocator
+      .registerFactory<FilterBloc>(() => FilterBloc(serviceLocator()));
+
+  serviceLocator
+      .registerLazySingleton(() => FilterByCategorie(serviceLocator()));
 }
