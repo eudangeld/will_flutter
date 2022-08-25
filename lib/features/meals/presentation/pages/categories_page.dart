c6608a6c490a7e84e0839b12aeff0d114d699ae6
dan <eudangeld@gmail.com>
2022-08-25 07:56:05 -0300

    feat: Categories page done

--- lib/features/meals/presentation/pages/categories_page.dart
+++ lib/features/meals/presentation/pages/categories_page.dart
@@ -1,14 +1,40 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:will_flutter/features/meals/presentation/bloc/categories/categories_bloc.dart';
 
 class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('The meal App'),
-        ),
-        // floatingActionButton: InkWell(onTap: ()async => ,),
-        body: const Center(child: Text('Hello world')));
+      appBar: AppBar(
+        title: const Text('The meal App'),
+      ),
+      body: BlocBuilder<CategoriesBloc, CategoriesState>(
+        builder: (context, state) {
+          if (state is CategoriesLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (state is CategoriesEmptyState) {
+            BlocProvider.of<CategoriesBloc>(context).add(GetCategoriesEvent());
+          }
+          final categories = state.categories;
+          return ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse,
+            }),
+            child: ListView.builder(
+              physics: const AlwaysScrollableScrollPhysics(),
+              itemCount: categories.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  ListTile(title: Text(categories[index].title)),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
